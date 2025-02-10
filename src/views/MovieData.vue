cbf1284da4db5ca7c527e02ef877f560f08851a0
Rouf Majidin <roufmajidin777@gmail.com>
2025-02-11 03:01:09 +0700

    read movie and sat list

--- src/views/MovieData.vue
+++ src/views/MovieData.vue
@@ -1,3 +1,65 @@
+
+<script>
+import { ref, onMounted, computed } from "vue";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+export default {
+  /**
+   *
+   */
+  setup() {
+    const baseurl = "http://localhost:4000";
+    const movie = ref([]);
+    const seats = ref([]);
+    const showSeat =async (seat, idWaktu) =>{
+    const response = await fetch(`${baseurl}/movies/seats/${seat}/${idWaktu}`);
+    const data = await response.json();
+
+    if (data?.data?.seat) {
+          seats.value = data.data.seat;
+        } else {
+          seats.value = [];
+        }
+    console.log(seat + "," + idWaktu);
+    console.log(seats);
+
+    }
+    const groupedSeats = computed(() => {
+        return seats.value.reduce((acc, seat) => {
+          if (!acc[seat.row]) acc[seat.row] = [];
+          acc[seat.row].push(seat);
+          return acc;
+        }, {});
+      });
+      const formatWIB = (time) => {
+        return dayjs.utc(time).tz("Asia/Jakarta").format("DD/MM/YYYY HH:mm:ss");
+      };
+
+
+
+    const fetchmovie = async () => {
+      try {
+        const response = await fetch(baseurl+"/movies/movies");
+        const data = await response.json();
+        // users.value = data;
+        movie.value = data;
+        console.log(movie)
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+
+    onMounted(fetchmovie);
+
+    return { movie, formatWIB, showSeat, seats, groupedSeats };
+  },
+};
+</script>
+
 <template>
 
 <div class="hk-pg-wrapper">
@@ -17,23 +79,47 @@
                                                     <th>Judul</th>
                                                     <th>Office</th>
                                                     <th>Age</th>
-                                                    <th>Start date</th>
+                                                    <th>Dimulai</th>
+                                                    <th>Duration</th>
+                                                    <th>action</th>
 
 
-                                                    <th>Salary</th>
                                                 </tr>
                                             </thead>
                                             <tbody>
-                                                <tr>
-                                                    <td>#</td>
-                                                    <td>System Architect</td>
-                                                    <td>Edinburgh</td>
-                                                    <td>61</td>
-                                                    <td>2011/04/25</td>
-                                                    <td>$320,800</td>
+                                              <tr v-for="(item, index) in movie" :key="item.id">
+                                                <td>{{ index + 1 }}</td>
+                                                <td>{{ item.movies.judul }}</td>
+                                                <td>{{ item.movies.genre }}</td>
+                                                <td>{{ item.rooms.name }}</td>
+                                                <td>{{ new Date(item.time).toLocaleString("id-ID", { timeZone: "Asia/Jakarta" }) }}</td>
+                                                <td>{{ item.movies.durasi }} min</td>
+                                                <td>{{ item.movie_id }} min</td>
+                                                <td>
+                                                  <button type="button"  @click="showSeat(item.room_id, item.id)"  data-toggle="dropdown" class="btn btn-secondary dropdown-toggle" >seat</button>
 
+                                                <div class="dropdown-menu dropdown-authentication">
+                                                  <template v-for="(seats, row) in groupedSeats" :key="row">
+                                                  <div class="d-flex align-items-center">
+                                                    <span class="font-weight-bold me-2" style="margin: 2px">{{ row }} </span>
+                                                    <div class="d-flex flex-wrap">
+                                                      <button
+                                                        v-for="seat in seats"
+                                                        :key="seat.id"
+                                                        type="button" style="margin: 4px;"
+                                                        :class="{
+                                                          'btn btn-secondary': seat.Booking.length === 0,
+                                                          'btn btn-primary': seat.Booking.length > 0
+                                                        }">
+                                                        {{ seat.number }}
+                                                      </button>
+                                                    </div>
+                                                        </div>
+                                                      </template>
+                                                    </div>
 
-                                                  </tr>
+                                                </td>
+                                              </tr>
 
 
                                             </tbody>
@@ -52,3 +138,4 @@
             <!-- Footer -->
 
 </template>
+
